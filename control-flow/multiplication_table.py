99fc78eb34fa758257482e67b972243418a23c89
Richard Dok <Richard.Dok@myenterprisegroup.io>
2024-09-20 21:20:07 +0000

    Week 4 project 2 for loop

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,7 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1,11):
-    Z=number*i
-    print(number," *", i, "=",Z)
+    
+    print(number," *", i, "=",number*i)
     i=i+1
 
