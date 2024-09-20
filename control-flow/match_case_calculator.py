32316c81adbb218ad30c791539d4e013506af214
Richard Dok <Richard.Dok@myenterprisegroup.io>
2024-09-20 21:01:47 +0000

    Week 4 project 2 match cas calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,10 +1,10 @@
 num1= float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-opr = input("Choose the operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
-match opr:
+match operation:
     case "+":
-        result=num1+num2
+        result=num1 + num2
     case "-":
         result = num1 - num2
         print("The result is", result)
@@ -15,7 +15,7 @@ match opr:
         if  num2 == 0:
             print("Cannot divide by zero")
         else:
-            result=num1/num2
+            result=num1 / num2
             print("The result is", result)
     case _:
         print("Your input not in the required operations listed")
