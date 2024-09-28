0ba0c8f6181996c87e3811f8d3d44c5831ea9dc0
Richard Dok <Richard.Dok@myenterprisegroup.io>
2024-09-28 23:17:03 +0000

    add the files

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,15 @@
 # define a function perform_operation
-def perform_operation(num1,num2,operation):
+def perform_operation(num1, num2, operation):
     if operation == "add":
-        return num1+num2
+        return num1 + num2
     elif operation == "subtract":
-        return num1-num2
+        return num1 - num2
     elif operation == "multiply":
-        return num1*num2
+        return num1 * num2
     elif operation == "divide":
         if  num2==0:
             return print("We can nor divide by zero")
         else: 
-            return num1/num2
+            return num1 / num2
     else:
         return print("No proper operation inputted")
