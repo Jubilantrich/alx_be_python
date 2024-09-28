5eed58d454c079a2340773a20abcdedd0d0e7be6
Richard Dok <Richard.Dok@myenterprisegroup.io>
2024-09-28 04:51:51 +0000

    practice modules and packages

---
+++ Practice_modules_packages/calculator.py
@@ -0,0 +1,16 @@
+# I am going to create a list of functions to be used later.
+
+def addition(num1,num2): # addition functions
+    #return the addition of the two arguement
+    return num1+num2
+
+def subtract(num1, num2): #subtraction fuction
+    #returns the diff of num1 - num 2
+    return num1 - num2
+
+def multiplication(num1, num2): # multiplication functions
+    #returns the product of num1 and num2
+    return num1*num2
+def division(num1,num2): #divide two numbers
+    #returs the results of the dividion
+    return num1/num2
---
+++ Practice_modules_packages/main.py
@@ -0,0 +1,14 @@
+# want to make good use of the fuctions in the calcultor module
+import calculator
+
+Addition_results = calculator.addition(15,58)
+print("Addition Results is", Addition_results)
+
+Multiplication_results = calculator.multiplication(19,73)
+print("Multiplication Results is", Multiplication_results)
+
+Subtract_results = calculator.subtract(112,73)
+print("Subtraction Results is", Subtract_results)
+
+Divide_results = calculator.division(112,73)
+print("Division Results is", Divide_results)
