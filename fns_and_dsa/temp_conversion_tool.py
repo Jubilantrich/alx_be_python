1749f45a5c0d4be3c264a85251474aafd0e7a7ed
Richard Dok <Richard.Dok@myenterprisegroup.io>
2024-09-29 10:38:42 +0000

    Tempreture and conversion tool project added

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -4,11 +4,11 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    convert_to_celsius=fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
+    convert_to_celsius=fahrenheit-32 * FAHRENHEIT_TO_CELSIUS_FACTOR
     return convert_to_celsius
 
 def convert_to_fahrenheit(celsius):
-    convert_to_fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR 
+    convert_to_fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
     return  convert_to_fahrenheit
 
 Celsius_or_Fahrenheit_Value= float(input("Enter the temperature to convert: "))
